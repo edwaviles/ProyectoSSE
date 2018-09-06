af3c4607a094622720943e075aeea2724e89e8e5
reniery <reniery@Escalante>
2018-09-05 21:23:17 -0600

    uniendo casi lo ultimo 2.0

--- src/edu/dao/DaoNotificacion.java
+++ src/edu/dao/DaoNotificacion.java
@@ -3,6 +3,8 @@ package edu.dao;
 import java.sql.*;
 
 import edu.conexion.Conexion;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Nombre de la clase: DaoNotificacion
@@ -43,5 +45,60 @@ public class DaoNotificacion extends Conexion{
         }
         return ls;    
     }
+    
+    public List getSSE(int id)
+    {
+        ResultSet res;
+        List ls=new ArrayList();
+        try 
+        {
+            this.conectar();
+            String sql="select s.idSolicitudSSE as id from solicitudsse s inner join "
+                    + "alumno a on s.idAlumno= a.idAlumno INNER JOIN carrera c "
+                    + "on c.idCarrera=a.carrera INNER JOIN coordinadorsse cor "
+                    + "on cor.carrera_idCarrera=c.idCarrera "
+                    + "WHERE cor.idCoordinador=? and s.estado=0";
+            PreparedStatement pre=getCon().prepareStatement(sql);
+            pre.setInt(1, id);
+            res=pre.executeQuery();
+            while (res.next()) 
+            {                
+                ls.add(res.getInt("id"));
+            }
+            
+        } 
+        catch (SQLException e) 
+        {
+            
+        }
+        finally
+        {
+            this.desconectar();
+        }
+        return ls;    
+    }
+    
+     public void elimiarVistos(int id)
+    {
+        try 
+        {
+            this.conectar();
+            String sql="UPDATE `solicitudsse` SET `estado` = '1' WHERE `solicitudsse`.`idSolicitudSSE` = ?;";
+            PreparedStatement pre=getCon().prepareStatement(sql);
+            pre.setInt(1, id);
+            pre.executeUpdate();                        
+        } 
+        catch (SQLException e) 
+        {
+            
+        }
+        finally
+        {
+            this.desconectar();
+        }
+    }
+    
+    
+    
        
 }
