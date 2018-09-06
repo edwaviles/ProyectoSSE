394fba2344229f6b36f4a53e752a8b9d5131e87c
Escalante <Escalante@DESKTOP-UCP7B19>
2018-09-06 03:30:49 -0600

    validaciones de visibilidad

--- src/edu/vistas/Menu.java
+++ src/edu/vistas/Menu.java
@@ -48,9 +48,7 @@ public class Menu extends javax.swing.JFrame {
         setIconImage(new ImageIcon(getClass().getResource("addd.png")).getImage());
         this.setExtendedState(MAXIMIZED_BOTH); 
         } 
-    
- 
-    
+  
     public void correr()
     {        
         SwingUtilities.invokeLater(new Runnable()
@@ -251,15 +249,22 @@ public class Menu extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void GestionCoordinadorMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_GestionCoordinadorMouseClicked
-        if(FormularioVal==false && idUs==2)
+        if(FormularioVal==false)
         {
-            this.gestionC = new FrmCoordinador();
-            this.desktopPane.add(gestionC);
-            gestionC.setVisible(true);
-            gestionC.setLocation(
-            desktopPane.getWidth()/2 - gestionC.getWidth()/2,
-            desktopPane.getHeight()/2 - gestionC.getHeight()/2);
-            FormularioVal=true;
+            if(idUs==1||idUs==3)
+            {
+                this.gestionC = new FrmCoordinador();
+                this.desktopPane.add(gestionC);
+                gestionC.setVisible(true);
+                gestionC.setLocation(
+                desktopPane.getWidth()/2 - gestionC.getWidth()/2,
+                desktopPane.getHeight()/2 - gestionC.getHeight()/2);
+                FormularioVal=true;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"No autorizado");
+            }           
         }
         else
         {
@@ -275,13 +280,20 @@ public class Menu extends javax.swing.JFrame {
     private void GestionHorariosMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_GestionHorariosMouseClicked
         if(FormularioVal==false)
         {
-            this.gestionH = new FrmHorario();
-            this.desktopPane.add(gestionH);
-            gestionH.setVisible(true);
-            gestionH.setLocation(
-                    desktopPane.getWidth()/2 - gestionH.getWidth()/2,
-                    desktopPane.getHeight()/2 - gestionH.getHeight()/2);
-                    FormularioVal=true;
+             if(idUs==2)
+            {
+                this.gestionH = new FrmHorario();
+                this.desktopPane.add(gestionH);
+                gestionH.setVisible(true);
+                gestionH.setLocation(
+                desktopPane.getWidth()/2 - gestionH.getWidth()/2,
+                desktopPane.getHeight()/2 - gestionH.getHeight()/2);
+                FormularioVal=true;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"No autorizado");
+            }
         }
         else
         {
@@ -317,14 +329,22 @@ public class Menu extends javax.swing.JFrame {
 
     private void solicitudesMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_solicitudesMouseClicked
         if(FormularioVal==false)
-        {            
-            this.gestionS = new FrmSSSE();
-            this.desktopPane.add(gestionS);
-            gestionS.setVisible(true);
-            gestionS.setLocation(
-            desktopPane.getWidth()/2 - gestionS.getWidth()/2,
-            desktopPane.getHeight()/2 - gestionS.getHeight()/2);
-            FormularioVal=true;
+        {    
+             if(idUs!=1||idUs!=3)
+            {
+                this.gestionS = new FrmSSSE();
+                this.desktopPane.add(gestionS);
+                gestionS.setVisible(true);
+                gestionS.setLocation(
+                desktopPane.getWidth()/2 - gestionS.getWidth()/2,
+                desktopPane.getHeight()/2 - gestionS.getHeight()/2);
+                FormularioVal=true;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"No autorizado");
+            }
+           
         }
         else
         {
@@ -335,13 +355,21 @@ public class Menu extends javax.swing.JFrame {
     private void GestionarUsuarioMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_GestionarUsuarioMouseClicked
         if(FormularioVal==false)
         {
-            this.gestionU = new FrmUsuario();
-            this.desktopPane.add(gestionU);
-            gestionU.setVisible(true);
-            gestionU.setLocation(
-            desktopPane.getWidth()/2 - gestionU.getWidth()/2,
-            desktopPane.getHeight()/2 - gestionU.getHeight()/2);
-            FormularioVal=true;
+             if(idUs==1||idUs==3)
+            {
+                this.gestionU = new FrmUsuario();
+                this.desktopPane.add(gestionU);
+                gestionU.setVisible(true);
+                gestionU.setLocation(
+                desktopPane.getWidth()/2 - gestionU.getWidth()/2,
+                desktopPane.getHeight()/2 - gestionU.getHeight()/2);
+                FormularioVal=true;
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"No autorizado");
+            }
+            
         }
         else
         {
